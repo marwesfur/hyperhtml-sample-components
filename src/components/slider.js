e5393f5410ab3c18b983ceb1740b8aea16b640a4
Markus Westphal-Furuya <mail@markuswestphal.de>
2018-08-09 09:09:31 +0200

    added stepper button, display only selected slide, fire event on slide change

--- src/components/slider.js
+++ src/components/slider.js
@@ -15,13 +15,28 @@ class BioComponent extends HyperHTMLElement {
     // when setting "props" as a property, exepct an object
     set props(value) {
         this._props = value;
-        this.render(); // assume, that an element is already in the dom and we change its props -- then we need to re-render...
-        // when going through attributes, HyperHTMLElement automatically triggers render for us
+        if (!this._init$) {
+            this.render(); // assume, that an element is already in the dom and we change its props -- then we need to re-render...
+            // when going through attributes, HyperHTMLElement automatically triggers render for us
+        }
     }
 }
 
 
 class Slide extends BioComponent {
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    render() {
+        const isSelected = this.props && this.props.isSelected;
+
+        return isSelected
+            ? this.html`<div class="slide"><slot></slot></div>`
+            : this.html`<div class="slide"></div>`;
+    }
 
 }
 
@@ -35,15 +50,15 @@ class Slider extends BioComponent {
         // re-render when children have changed. Note that childNodes is empty when connectedCallback is called
         // note also, that created() is called when initial childNodes are available. so maybe only re-render
         // on child note changes after created has been called
-        const observer = new MutationObserver(changes => this.render());
+        const observer = new MutationObserver(changes => !this._init$ && this.render());
         observer.observe(this, {childList: true});
+
+        this.onNextSlide = this.onNextSlide.bind(this);
+        this.onPrevSlide = this.onPrevSlide.bind(this);
+        this.onSelectSlide = this.onSelectSlide.bind(this);
     }
 
-    attributeChangedCallback(name, prev, curr) {
-        this.render();
-    }
-
-    connectedCallback() {
+    created() {
         this.render();
     }
 
@@ -52,22 +67,63 @@ class Slider extends BioComponent {
     }
 
     render() {
-        const slides = [...this.childNodes].filter((n) => n instanceof Slide);
-        const slidesCount = slides.length;
         const props = this.props || this.defaultProps;
+        const slidesCount = this.slidesCount;
+
+        this.slides.forEach((slide, idx) => slide.props = {isSelected: idx === props.selected});
 
         return this.html`
+            <x-stepper direction="prev" onclick=${this.onPrevSlide}></x-stepper>
             <div class="slides">
                 <slot></slot>
             </div>
+            <x-stepper direction="next" onclick=${this.onNextSlide}></x-stepper>
             <x-dots 
                 props=${ {count: slidesCount, selected: props.selected} }
-                onselectSlide=${e => this.onSelectSlide(e)}></x-dots>
+                onselectSlide=${this.onSelectSlide}></x-dots>
         `;
     }
 
     onSelectSlide(e) {
         this.props = {selected: e.detail};
+
+        this.raiseSlideChange();
+    }
+
+    onPrevSlide() {
+        const {selected} = this.props || this.defaultProps;
+        const slidesCount = this.slidesCount;
+
+        this.props = {selected: (selected - 1 + slidesCount) % slidesCount };
+
+        this.raiseSlideChange();
+    }
+
+    onNextSlide() {
+        const {selected} = this.props || this.defaultProps;
+        const slidesCount = this.slidesCount;
+
+        this.props = {selected: (selected + 1) % slidesCount };
+
+        this.raiseSlideChange();
+    }
+
+    raiseSlideChange() {
+        const changeEvent = new CustomEvent('slideChange', {
+            bubbles: true,
+            cancelable: true,
+            detail: this.props.selected
+        });
+
+        this.dispatchEvent(changeEvent);
+    }
+
+    get slides() {
+        return [...this.childNodes].filter((n) => n instanceof Slide);
+    }
+
+    get slidesCount() {
+        return this.slides.length;
     }
 }
 
@@ -75,20 +131,16 @@ Slider.define('x-slider');
 
 class Dots extends BioComponent {
 
-    connectedCallback() {
-        this.render();
-    }
-
-    attributeChangedCallback(name, prev, curr) {
+    created() {
         this.render();
     }
 
     render() {
-        const {selected} = this.props;
-        const dots = new Array(this.props.count).fill(1);
+        const {selected, count} = this.props;
+        const dots = new Array(count).fill(1);
 
         const dotElements = dots.map((_, idx) => selected === idx
-            ? BioComponent.wire()`<li onclick=${() => this.onSelected(idx)}><b>+</b></li>`
+            ? BioComponent.wire()`<li onclick=${() => this.onSelected(idx)} style="color: red">+</li>`
             : BioComponent.wire()`<li onclick=${() => this.onSelected(idx)}>+</li>`);
 
         this.html`
@@ -108,3 +160,19 @@ class Dots extends BioComponent {
 }
 
 Dots.define('x-dots');
+
+
+class Stepper extends BioComponent {
+
+    created() {
+        this.render();
+    }
+
+    render() {
+        this.html`
+            <button>${this.getAttribute('direction')}</button>
+        `;
+    }
+}
+
+Stepper.define('x-stepper');
